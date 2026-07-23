9cbcd654b617e07d0900d36f398f55a194c69a16
Your Name <your@email.com>
2026-07-23 22:04:23 +0300

    Complete lab

--- index.js
+++ index.js
@@ -4,3 +4,35 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+
+require('datejs');
+
+
+function combineUsers(...args) {
+    
+
+    const combinedObject = {
+        users: []
+    };
+
+
+    args.forEach(userArray => {
+
+        combinedObject.users = [...combinedObject.users, ...userArray];
+    });
+
+    
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+   
+    return combinedObject;
+}
+
+
+const siteA = ['user1', 'user2'];
+const siteB = ['user3', 'user4'];
+const siteC = ['user5'];
+
+const mergedData = combineUsers(siteA, siteB, siteC);
+console.log(mergedData);
